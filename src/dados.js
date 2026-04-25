71e05dac1dfa6c100eb3e4cd55449ee16254b680
fabianooliveira-commits <fabiano.oliveira@pditabira.com.br>
2026-04-25 00:53:58 -0300

    Add files via upload

---
+++ src/dados.js
@@ -0,0 +1,142 @@
+let produtosLoja = [ 
+        {
+        id: "Joia1",
+        nome: "Aneis ouro Branco",
+        preco: 200,
+        descricao: "Anel luxoso para uso diario.",
+        img: "img/produto-1.jpg"
+    },
+        {
+        id: "roupa1",
+        nome: "Jaqueta Roxa",
+        preco: 90,
+        descricao: "jaqueta confortavel para uso diario.",
+        img: "img/produto-2.jpg"
+    },
+          {
+        id: "tecnologia1",
+        nome: "Placa de video",
+        preco: 450,
+        descricao: "Placa de video para computadores.",
+        img: "img/produto-3.jpg"
+    },
+          {
+        id: "roupa2",
+        nome: "Jaqueta beje",
+        preco: 45,
+        descricao: "jaqueta rustica para uso diario.",
+        img: "img/produto-4.jpg"
+    },
+          {
+        id: "joia2",
+        nome: "pulseira prata",
+        preco: 65,
+        descricao: "Pulseira prata adorno de dragão.",
+        img: "img/produto-5.jpg"
+    },
+          {
+        id: "roupa3",
+        nome: "camisa Casual",
+        preco: 25,
+        descricao: "camisa confortavel para uso diario.",
+        img: "img/produto-6.jpg"
+    },
+          {
+        id: "joia3",
+        nome: "Anel de Diamante",
+        preco: 200,
+        descricao: "Anel elegante com grande pedra.",
+        img: "img/produto-7.jpg"
+    },
+          {
+        id: "roupa4",
+        nome: "Camisa manga longa",
+        preco: 45,
+        descricao: "Camisa manga longa confortavel para uso diario.",
+        img: "img/produto-8.jpg"
+    },
+         {
+        id: "roupa4",
+        nome: "Camisa longa casual",
+        preco: 45,
+        descricao: "Camisa longa confortavel para uso diario.",
+        img: "img/produto-9.jpg"
+    },
+         {
+        id: "roupa5",
+        nome: "Bolsa moderna",
+        preco: 45,
+        descricao: "Bolsa moderna confortavel para uso diario.",
+        img: "img/produto-10.jpg"
+    },
+         {
+        id: "tecnologia2",
+        nome: "televisão tela plana",
+        preco: 1300,
+        descricao: "televisão tela plana dinamica.",
+        img: "img/produto-11.jpg"
+    },
+         {
+        id: "tecnologia3",
+        nome: "HD elements",
+        preco: 70,
+        descricao: "HD elements de grande capacidade.",
+        img: "img/produto-12.jpg"
+    },
+         {
+        id: "tecnologia4",
+        nome: "HD externo moderno com led",
+        preco: 65,
+        descricao: "Camisa manga longa confortavel para uso diario.",
+        img: "img/produto-13.jpg"
+    },
+         {
+        id: "roupa6",
+        nome: "Camisa be kind",
+        preco: 45,
+        descricao: "Camisa confortavel para uso diario.",
+        img: "img/produto-14.jpg"
+    },
+         {
+        id: "joia4",
+        nome: "pulseira prata com diamantes",
+        preco: 4500,
+        descricao: "pulseira prata luxo adornada com diamantes.",
+        img: "img/produto-15.jpg"
+    },
+         {
+        id: "tecnologia5",
+        nome: "SSD Plus",
+        preco: 45,
+        descricao: "SSD Plus solid state drive SanDisk.",
+        img: "img/produto-16.jpg"
+    },
+         {
+        id: "roupa7",
+        nome: "Camisa gola v",
+        preco: 45,
+        descricao: "camisa gola v confortavel para uso diario.",
+        img: "img/produto-17.jpg"
+    },
+         {
+        id: "roupa8",
+        nome: "Sobretudo",
+        preco: 45,
+        descricao: "Sobretudo confortavel para dias frios.",
+        img: "img/produto-18.jpg"
+    },
+         {
+        id: "roupa9",
+        nome: "Jaqueta preta corta vento",
+        preco: 45,
+        descricao: "Jaqueta preta corta vent confortavel para dias frios.",
+        img: "img/produto-19.jpg"
+    },
+         {
+        id: "tecnologia6",
+        nome: "Oled gamer monitor",
+        preco: 45,
+        descricao: "Monitor longo e dobravel para gamers.",
+        img: "img/produto-20.jpg"
+    },
+]
